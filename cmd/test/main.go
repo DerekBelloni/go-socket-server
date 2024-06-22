e0a5276070f3d1bf9a15264fe1df43a8b0dab520
Derek Belloni <derekbelloni@Dereks-MacBook-Pro.local>
2024-06-22 15:27:19 -0600

    cleaned up comments

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -17,15 +17,11 @@ func main() {
 		"wss://nos.lol",
 	}
 
-	// channel to communicate that notes have been set in redis
 	finished := make(chan string)
-	// channel to communicate to the wait group that all go routines are done
 	done := make(chan bool)
 	var wg sync.WaitGroup
 
-	// instantiate a timer
 	ticker := time.NewTicker(10 * time.Minute)
-	// makes sure the ticker is stopped when the main function exits, important for resource clean up
 	defer ticker.Stop()
 
 	for _, relayUrl := range relayUrls {
@@ -42,25 +38,17 @@ func main() {
 					return
 				}
 			}
-			// Allows calling the anon function with the current iteration in relayUrls
-			// Each GO routine has its own copy
 		}(relayUrl)
 	}
 
-	// Listen for OS signals to gracefully shutdown
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		// Block until a signal is received
 		<-sig
-		// signal to close the all go routines created in the loop
 		close(done)
-		// wait for go routines to finish
 		wg.Wait()
-		// close the wait group
 		wg.Done()
-		// exit the program
 		os.Exit(0)
 	}()
 
