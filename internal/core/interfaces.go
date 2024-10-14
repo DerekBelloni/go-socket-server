1914a15eba2a8052ba212abd325a1efb94279ebc
Derek Belloni <derekbelloni10@gmail.com>
2024-10-13 19:55:25 -0600

    refactored file structure

--- internal/core/interfaces.go
+++
@@ -1,9 +0,0 @@
-package core
-
-type RelayConnector interface {
-	GetConnection(relayUrl string) (chan []byte, chan string, error)
-	GetUserMetadata(relayUrl string, userHexKey string, metadataFinished chan<- string)
-	GetUserNotes(relayUrl string, userHexKey string, notesFinished chan<- string)
-	GetFollowList(relayUrl string, userHexKey string, followsFinished chan<- string)
-	GetFollowListMetadata(relayUrl string, userHexKey string)
-}
--- internal/data/newNote.go
+++
@@ -1,8 +0,0 @@
-package data
-
-type NewNote struct {
-	PrivHexKey string `json:"privHexKey"`
-	PubHexKey  string `json:"pubHexKey"`
-	Kind       int    `json:"kind"`
-	Content    string `json:"content"`
-}
